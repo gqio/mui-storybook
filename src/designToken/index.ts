17ade61a49ba0779fa1536bae9eabae8520d90c3
Gregory Quinio <1063530+undefined@users.noreply.github.com>
2022-07-25 14:08:33 +0000

    Push from aa

--- src/designToken/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './paletteNameList'
-export * from './createDesignToken'
-export { default as createDesignToken } from './createDesignToken'
-export * from './selector'
-export * from './types'
-export * from './DesignTokenProvider'
